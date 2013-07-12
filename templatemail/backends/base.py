a8e24acc7d9596ccc2b9e7d05a32664d780e3fab
Tim Downs <tdowns@urbancode.com>
2013-07-12 13:08:59 -0400

    handle it when the email is paired with template

--- templatemail/backends/base.py
+++ templatemail/backends/base.py
@@ -12,29 +12,33 @@ class BaseEmailBackend(object):
         emails = []
         for message in email_messages:
             body = message.body
-            assert (type(body) is tuple or type(body) is list), "The message must be a tuple. e.g ('template_directory/template.html',)"
-            template_name = body[0]
-            dictonnary = len(body) >= 2 and body[1] or None
-            context_instance = len(body) >= 3 and body[2] or None
-            html_content = render_to_string(template_name, dictonnary, context_instance)
-
-            path = settings.EMAIL_HTML2TEXT
-            if path is None:
-                html2text = self._html2text
+            if (type(body) is str):
+                plain_content = body
+                html_content = body
             else:
-                try:
-                    mod_name, method_name = path.rsplit('.', 1)
-                    mod = import_module(mod_name)
-                except ImportError, e:
-                    raise ImproperlyConfigured(('Error importing email backend module %s: "%s"'
-                                                % (mod_name, e)))
-                try:
-                    html2text = getattr(mod, method_name)
-                except AttributeError:
-                    raise ImproperlyConfigured(('Module "%s" does not define a '
-                                                '"%s" method' % (mod_name, method_name)))
+                assert (type(body) is tuple or type(body) is list), "The message must be a tuple. e.g ('template_directory/template.html',)"
+                template_name = body[0]
+                dictonnary = len(body) >= 2 and body[1] or None
+                context_instance = len(body) >= 3 and body[2] or None
+                html_content = render_to_string(template_name, dictonnary, context_instance)
 
-            plain_content = html2text(html_content)
+                path = settings.EMAIL_HTML2TEXT
+                if path is None:
+                    html2text = self._html2text
+                else:
+                    try:
+                        mod_name, method_name = path.rsplit('.', 1)
+                        mod = import_module(mod_name)
+                    except ImportError, e:
+                        raise ImproperlyConfigured(('Error importing email backend module %s: "%s"'
+                                                    % (mod_name, e)))
+                    try:
+                        html2text = getattr(mod, method_name)
+                    except AttributeError:
+                        raise ImproperlyConfigured(('Module "%s" does not define a '
+                                                    '"%s" method' % (mod_name, method_name)))
+
+                plain_content = html2text(html_content)
 
             msg = EmailMultiAlternatives(subject=message.subject, body=plain_content, from_email=message.from_email, to=message.to, bcc=message.bcc,
                  connection=message.connection, attachments=message.attachments, headers=message.extra_headers, cc=message.cc)
